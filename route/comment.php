98f56f4ec770bbccb53b268e40b8086506817cc3
yangbangchao <15819915273@163.com>
2026-08-06 16:25:43 +0800

    把日期改为最近的

--- route/comment.php
+++ route/comment.php
@@ -116,8 +116,8 @@ switch ($action) {
 
             if($user['gid'] == 1){
                 $n_uid = mt_rand(2922, 3486);
-                $end = strtotime('-3 month');
-                $start = strtotime('-10 month');
+                $end = time()-3600*24*1;
+                $start = strtotime('-3 month');
                 $n_time = mt_rand($start, $end);
                 $post = array('tid' => $tid, 'uid' => $n_uid, 'fid' => $fid, 'create_date' => $n_time, 'userip' => $longip, 'doctype' => $doctype, 'quotepid' => $quotepid, 'message' => $message);
 
