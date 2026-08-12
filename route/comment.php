cd7cd0d1768b8051acd3a39fa3735c9c75a2d4c1
yangbangchao <15819915273@163.com>
2026-08-12 19:25:56 +0800

    评论时间修改

--- route/comment.php
+++ route/comment.php
@@ -117,8 +117,8 @@ switch ($action) {
 
             if($user['gid'] == 1){
                 $n_uid = mt_rand(2922, 3486);
-                $end = strtotime('-6 month');
-                $start = strtotime('-24 month');
+                $end = time()-3600*24*1;
+                $start =  time()-3600*24*5;
                 $n_time = mt_rand($start, $end);
                 $post = array('tid' => $tid, 'uid' => $n_uid, 'fid' => $fid, 'create_date' => $n_time, 'userip' => $longip, 'doctype' => $doctype, 'quotepid' => $quotepid, 'message' => $message, 'rating' => $rating);
 
